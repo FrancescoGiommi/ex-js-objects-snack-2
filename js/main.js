c6fea5b8b9cd94712f9ef85cc4254270c0cd5daa
Francesco <you@example.com>
2025-02-25 16:58:38 +0100

    Add code question 1

--- js/main.js
+++ js/main.js
@@ -8,9 +8,9 @@ secondBurger.name = "Double Cheese Burger";
 
 secondBurger.weight = 500;
 
-console.log(hamburger.name); // ?
+console.log(hamburger.name); // L'oggetto creato è solo 1 ed entrambi fanno riferimento ad hamburger con nome Cheese Burger, poi con la modifica di secondBurger il nome è diventato Double Cheese Burger per entrambi
 
-console.log(secondBurger.name); // ?
+console.log(secondBurger.name); // L'oggetto creato è solo 1 ed entrambi fanno riferimento ad hamburger con nome Cheese Burger, poi con la modifica di secondBurger il nome è diventato Double Cheese Burger per entrambi
 
 /*Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
     Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?*/
