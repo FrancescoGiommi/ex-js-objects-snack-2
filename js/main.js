9fd36f74e43ba5b8c27b60c587666373dfcc04f6
Francesco <you@example.com>
2025-02-25 18:00:35 +0100

    Add code question 4

--- js/main.js
+++ js/main.js
@@ -8,9 +8,9 @@
 
 // secondBurger.weight = 500;
 
-// console.log(hamburger.name);  L'oggetto creato è solo 1 ed entrambi fanno riferimento ad hamburger con nome Cheese Burger, poi con la modifica di secondBurger il nome è diventato Double Cheese Burger per entrambi
+// console.log(hamburger.name);  //! L'oggetto creato è solo 1 ed entrambi fanno riferimento ad hamburger con nome Cheese Burger, poi con la modifica di secondBurger il nome è diventato Double Cheese Burger per entrambi
 
-// console.log(secondBurger.name);  L'oggetto creato è solo 1 ed entrambi fanno riferimento ad hamburger con nome Cheese Burger, poi con la modifica di secondBurger il nome è diventato Double Cheese Burger per entrambi
+// console.log(secondBurger.name);  //! L'oggetto creato è solo 1 ed entrambi fanno riferimento ad hamburger con nome Cheese Burger, poi con la modifica di secondBurger il nome è diventato Double Cheese Burger per entrambi
 
 /*Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
     Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?*/
@@ -29,9 +29,9 @@
 
 // secondBurger.ingredients[0] = "Salad";
 
-// console.log(hamburger.ingredients[0]);  Essendo come gli oggetti, entrambi fanno riferimento ad hamburgher.ingredients quindi, con la modifica di secondBurger in salad, ora per entrambi il primo ingrediente è Salad
+// console.log(hamburger.ingredients[0]);  //! Essendo come gli oggetti, entrambi fanno riferimento ad hamburgher.ingredients quindi, con la modifica di secondBurger in salad, ora per entrambi il primo ingrediente è Salad
 
-// console.log(secondBurger.ingredients[0]);  Essendo come gli oggetti, entrambi fanno riferimento ad hamburgher.ingredients quindi, con la modifica di secondBurger in salad, ora per entrambi il primo ingrediente è Salad
+// console.log(secondBurger.ingredients[0]);  //! Essendo come gli oggetti, entrambi fanno riferimento ad hamburgher.ingredients quindi, con la modifica di secondBurger in salad, ora per entrambi il primo ingrediente è Salad
 
 /* P.S.: Ricordati che gli Array, come gli oggetti, sono dei Reference Type (Tipi di Riferimento)!
 
@@ -40,30 +40,62 @@
 
 //!  Code Question 3
 
-const hamburger = {
-  name: "Cheese Burger",
+// const hamburger = {
+//   name: "Cheese Burger",
 
-  weight: 250,
+//   weight: 250,
 
-  maker: {
-    name: "Anonymous Chef",
+//   maker: {
+//     name: "Anonymous Chef",
 
-    restaurant: {
-      name: "Hyur's Burgers",
+//     restaurant: {
+//       name: "Hyur's Burgers",
 
-      address: "Main Street, 123",
+//       address: "Main Street, 123",
 
-      isOpen: true,
-    },
+//       isOpen: true,
+//     },
 
-    age: 29,
-  },
-};
+//     age: 29,
+//   },
+// };
 
-const secondBurger = structuredClone(hamburger);
+// const secondBurger = structuredClone(hamburger);
 
-const thirdBurger = structuredClone(hamburger);
+// const thirdBurger = structuredClone(hamburger);
 
 // Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
 
-// In questo caso sono stati creati 3 oggetti perchè è stato utilizzato structuredClone che permette di clonare gli oggetti e di modificare gli oggetti annidati
+//! In questo caso sono stati creati 3 oggetti perchè è stato utilizzato structuredClone che permette di clonare gli oggetti e di modificare gli oggetti annidati
+
+//! Code Question 4
+
+const chef = {
+  name: "Chef Hyur",
+
+  age: 29,
+
+  makeBurger: (num = 1) => {
+    console.log(`Ecco ${num} hamburger per te!`);
+  },
+};
+
+const restaurant = {
+  name: "Hyur's Burgers",
+
+  address: {
+    street: "Main Street",
+
+    number: 123,
+  },
+
+  openingDate: new Date(2025, 3, 11),
+
+  isOpen: false,
+};
+
+// Qual è il metodo migliore per clonare l’oggetto chef, e perché?
+//! Il metodo migliore è lo spread perchè possiamo creare una copia solo superficiale e permette di copiare anche le funzioni
+
+// Qual è il metodo migliore per clonare l’oggetto restaurant, e perché?
+//! Per copiare restaurant il metodo migliore è structuredClone perchè permette di creare cloni di oggetti e oggetti annidati
