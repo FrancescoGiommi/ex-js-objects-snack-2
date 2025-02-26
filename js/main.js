eecd3301458cfaa6b63a443dba09633065e92a94
Francesco <you@example.com>
2025-02-26 10:11:18 +0100

    Add code question 6

--- js/main.js
+++ js/main.js
@@ -102,45 +102,80 @@
 
 //! Code Question 5 (Bonus)
 
-const hamburger = {
-  name: "Cheese Burger",
+// const hamburger = {
+//   name: "Cheese Burger",
 
-  weight: 250,
+//   weight: 250,
 
-  maker: {
-    name: "Anonymous Chef",
+//   maker: {
+//     name: "Anonymous Chef",
 
-    restaurant: {
-      name: "Hyur's Burgers",
+//     restaurant: {
+//       name: "Hyur's Burgers",
 
-      address: "Main Street, 123",
+//       address: "Main Street, 123",
 
-      isOpen: true,
-    },
+//       isOpen: true,
+//     },
 
-    age: 29,
-  },
-};
+//     age: 29,
+//   },
+// };
 
-const newRestaurant = { ...hamburger.maker.restaurant };
+// const newRestaurant = { ...hamburger.maker.restaurant };
 
-newRestaurant.name = "Hyur's II";
+// newRestaurant.name = "Hyur's II";
 
-newRestaurant.address = "Second Street, 12";
+// newRestaurant.address = "Second Street, 12";
 
-const secondBurger = { ...hamburger };
+// const secondBurger = { ...hamburger };
 
-secondBurger.maker.restaurant = newRestaurant;
+// secondBurger.maker.restaurant = newRestaurant;
 
-secondBurger.maker.name = "Chef Hyur";
+// secondBurger.maker.name = "Chef Hyur";
 
-console.log(hamburger.maker.name); // Qua viene stampato "Chef Hyur" perchè è stato modificato il nome da secondBurger senza copiare maker
+//console.log(hamburger.maker.name); Qua viene stampato "Chef Hyur" perchè è stato modificato il nome da secondBurger senza copiare maker
 
-console.log(secondBurger.maker.name); // Qua viene stampato "Chef Hyur" perchè viene modificato il nome da secondBurger
+//console.log(secondBurger.maker.name);  Qua viene stampato "Chef Hyur" perchè viene modificato il nome da secondBurger
 
-console.log(hamburger.maker.restaurant.name); // Qua viene stampato "Hyur's II" perchè è stato modificato il valore di name da newRestaurant senza farne la copia
+//console.log(hamburger.maker.restaurant.name);  Qua viene stampato "Hyur's II" perchè è stato modificato il valore di name da newRestaurant senza farne la copia
 
-console.log(secondBurger.maker.restaurant.name); // Qua viene stampato "Hyur's II" perchè è stato sovrascritto newRestaurant come restaurant di secondBurger
+//console.log(secondBurger.maker.restaurant.name); Qua viene stampato "Hyur's II" perchè è stato sovrascritto newRestaurant come restaurant di secondBurger
 
 // Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
 // Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+
+//! Code Question 6 (Bonus)
+
+const chef = {
+  name: "Chef Hyur",
+
+  age: 29,
+
+  makeBurger: (num = 1) => {
+    console.log(`Ecco ${num} hamburger per te!`);
+  },
+
+  restaurant: {
+    name: "Hyur's Burgers",
+
+    welcomeClient: () => {
+      console.log("Benvenuto!");
+    },
+
+    address: {
+      street: "Main Street",
+
+      number: 123,
+
+      showAddress: () => {
+        console.log("Main Street 123");
+      },
+    },
+
+    isOpen: true,
+  },
+};
+
+// Qual è il metodo migliore per clonare l’oggetto chef, e perché?
+//! Il modo migliore è utilizzare lo spread perchè permette di copiare le funzioni
